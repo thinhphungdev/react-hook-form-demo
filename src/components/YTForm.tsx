eeb953eed0c8d1acd9fa254f6003086e8fd2b6d7
thinhphungdev <phungthinh0946519356@gmail.com>
2023-12-17 16:49:49 +0700

    feat: useForm and register form field to be tracked by react hook form

--- src/components/YTForm.tsx
+++ src/components/YTForm.tsx
@@ -1,17 +1,21 @@
+import { useForm } from 'react-hook-form';
+
 export const YouTubeForm = () => {
+  const { register } = useForm();
+
   return (
     <div>
       <h1>YT Form</h1>
 
       <form>
         <label htmlFor='username'>Username</label>
-        <input type='text' id='username' name='username' />
+        <input type='text' id='username' {...register('username')} />
 
         <label htmlFor='email'>E-mail</label>
-        <input type='email' id='email' name='email' />
+        <input type='email' id='email' {...register('email')} />
 
         <label htmlFor='channel'>Channel</label>
-        <input type='text' id='channel' name='channel' />
+        <input type='text' id='channel' {...register('channel')} />
 
         <button>Submit</button>
       </form>
