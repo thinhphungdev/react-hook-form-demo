52bcd55402a972fba321d5ec95b7fbf3fbf76621
thinhphungdev <phungthinh0946519356@gmail.com>
2023-12-17 17:20:50 +0700

    feat: display validation message

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # React Hook Form Project Demo
 
 - To Install React hook form Dev tools: `npm i -D @hookform/devtools`
+
+################################################################
+#ReactHookForm to Manage Form state
+`register(filedName: string) method allow us to register a form field, its state will be managed and tracked by RHF`
+
+- return all the props like (ref, onChange, onBlur, name) to pass to an <input /> element
+  ################################################################
+
+#ReactHookForm for Form Validaton
--- src/components/YTForm.tsx
+++ src/components/YTForm.tsx
@@ -8,7 +8,8 @@ type FormValue = {
 };
 
 export const YouTubeForm = () => {
-  const { register, control, handleSubmit } = useForm<FormValue>();
+  const { register, control, handleSubmit, formState } = useForm<FormValue>();
+  const { errors } = formState;
 
   const onSubmit = (data: FormValue) => {
     console.log('Submitted', data);
@@ -18,15 +19,40 @@ export const YouTubeForm = () => {
     <div>
       <h1>YT Form</h1>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <label htmlFor='username'>Username</label>
-        <input type='text' id='username' {...register('username')} />
+        <input
+          type='text'
+          id='username'
+          {...register('username', {
+            required: 'Username is required',
+          })}
+        />
+        <p className='error'>{errors.username?.message}</p>
 
         <label htmlFor='email'>E-mail</label>
-        <input type='email' id='email' {...register('email')} />
+        <input
+          type='email'
+          id='email'
+          {...register('email', {
+            pattern: {
+              value:
+                /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+              message: 'Invalid email format',
+            },
+          })}
+        />
+        <p className='error'>{errors.email?.message}</p>
 
         <label htmlFor='channel'>Channel</label>
-        <input type='text' id='channel' {...register('channel')} />
+        <input
+          type='text'
+          id='channel'
+          {...register('channel', {
+            required: 'Channel is required',
+          })}
+        />
+        <p className='error'>{errors.channel?.message}</p>
 
         <button>Submit</button>
       </form>
